083ba7d4afecd13656ddb08fcad328d01f24148a
N3X2S <xyzxyzxyz12345lol@gmail.com>
2025-05-29 12:33:40 +0500

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TaskManager from '../components/TaskManager';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TaskManager />
+    </div>
+  );
+}
