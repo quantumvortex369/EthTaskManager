46875090a1a2df5bb1bfc3621b4bc779936aa942
N3X2S <xyzxyzxyz12345lol@gmail.com>
2025-05-29 12:34:08 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TaskManager from './components/TaskManager'
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TaskManager />
+  </React.StrictMode>
+)
